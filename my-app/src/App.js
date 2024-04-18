aab8bc43d3befa0467fcfca6053f5087183222d9
Makgotso01 <111054182+Makgotso01@users.noreply.github.com>
2024-04-17 20:41:43 -0700

    updated js and css files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,27 +7,6 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather defaultCity="Johannesburg" />
-
-        <footer>
-          This project was coded by
-          <a href="" target="_blank" rel="noopener noreferrer">
-            Makgotso Ntai
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/Makgotso01/React-weatherappp"
-            target="_blank"
-            rel="noopener noreferrer">
-            open-sourced on GitHub
-          </a>{" "}
-          and{" "}
-          <a
-            href="https://kaleidoscopic-semolina-f30b47.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer">
-            hosted on Netlify
-          </a>
-        </footer>
       </div>
     </div>
   );
