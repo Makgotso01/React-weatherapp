4c321f97233e1cde321cbb59cb7b6c0c7014fdd1
Makgotso01 <111054182+Makgotso01@users.noreply.github.com>
2024-04-17 20:53:26 -0700

    updated css and js files

--- my-app/src/Weather.js
+++ my-app/src/Weather.js
@@ -11,14 +11,14 @@ export default function Weather(props) {
   function handleResponse(response) {
     setWeatherData({
       ready: true,
-      coordinates: response.data.coord,
-      temperature: response.data.main.temp,
-      humidity: response.data.main.humidity,
-      date: new Date(response.data.dt * 1000),
-      description: response.data.weather[0].description,
-      icon: response.data.weather[0].icon,
+      coordinates: response.data.coordinates,
+      temperature: response.data.temperature.current,
+      humidity: response.data.temperature.humidity,
+      date: new Date(response.data.time * 1000),
+      description: response.data.condition.description,
+      icon: response.data.condition.icon,
       wind: response.data.wind.speed,
-      city: response.data.name,
+      city: response.data.city,
     });
   }
 
@@ -33,25 +33,31 @@ export default function Weather(props) {
 
   function search() {
     const apiKey = "dd0a3a53b7tb8edf05dbd4ce6o42b46b";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
     axios.get(apiUrl).then(handleResponse);
   }
 
   if (weatherData.ready) {
     return (
       <div className="Weather">
+        <a
+          href="https://www.shecodes.io/"
+          target="_blank"
+          rel="noopener noreferrer">
+          <img src="/images/logo.png" className="logo" alt="SheCodes Logo" />
+        </a>
         <form onSubmit={handleSubmit}>
           <div className="row">
-            <div className="col-9">
+            <div className="col-9 ">
               <input
                 type="search"
                 placeholder="Enter a city.."
-                className="form-control"
-                autoFocus="on"
+                className="form-control search-input"
                 onChange={handleCityChange}
               />
             </div>
-            <div className="col-3">
+            <div className="col-3 p-0">
               <input
                 type="submit"
                 value="Search"
@@ -61,7 +67,33 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
-        <WeatherForecast coordinates={weatherData.coordinates} />
+        <WeatherForecast
+          coordinates={weatherData.coordinates}
+          city={weatherData.city}
+        />
+        <footer>
+          This project was coded by{"Makgotso Ntai "}
+          <a
+            href="https://www.shecodes.io/"
+            target="_blank"
+            rel="noopener noreferrer">
+            SheCodes
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Makgotso01/React-weatherappr"
+            target="_blank"
+            rel="noopener noreferrer">
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://kaleidoscopic-semolina-f30b47.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer">
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
     );
   } else {
